0a81f32a1c44b95be0ae3c3ac8287f476a69bfec
jwaller11 <conguy699@gmail.com>
2026-06-04 20:51:45 -0500

    Refactor polygon handling and console log statement

--- app.js
+++ app.js
@@ -138,26 +138,32 @@ function addFeature(rawFeature) {
   }
 
   if (f.type === "polygon") {
-  const positions = f.coordinates.map(cartesianFromCoord);
-  const outlinePositions = [...positions, positions[0]];
+    const positions = f.coordinates.map(c => Cesium.Cartesian3.fromDegrees(c[0], c[1], 0));
+    const outlinePositions = [...positions, positions[0]];
 
-  entity = viewer.entities.add({
-    ...common,
-    polygon: {
-      hierarchy: new Cesium.PolygonHierarchy(positions),
-      material: catColor.withAlpha(0.18),
-      outline: true,
-      outlineColor: catColor,
-      perPositionHeight: false,
-      height: 0
-    },
-    polyline: {
-      positions: outlinePositions,
-      width: 2,
-      material: catColor.withAlpha(0.9),
-      clampToGround: true
-    }
-  });
+    entity = viewer.entities.add({
+      ...common,
+      polygon: {
+        hierarchy: new Cesium.PolygonHierarchy(positions),
+        material: catColor.withAlpha(0.18),
+        outline: true,
+        outlineColor: catColor,
+        height: 0
+      },
+      polyline: {
+        positions: outlinePositions,
+        width: 2,
+        material: catColor.withAlpha(0.9),
+        clampToGround: false
+      }
+    });
+  }
+
+  if (entity) {
+    entity.tw4Feature = f;
+    if (entitiesByType[f.type]) entitiesByType[f.type].push(entity);
+    return entity;
+  }
 }
 
 DATA.features.forEach(addFeature);
